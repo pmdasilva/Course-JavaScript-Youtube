579cfa51a4e76601aeb3c3b110112aae0730889f
PauloSilva <pmdasilvaestud@gmail.com>
2026-07-18 18:14:13 -0300

    adicionando estilo com o classList.add do javscript ao inves do style.

--- JavaScriptBasic/DOM/toggle.js
+++ JavaScriptBasic/DOM/toggle.js
@@ -6,23 +6,23 @@ const conteudo = document.querySelector('.conteudo-header');
 
 
 // Isto ja deixa o conteudo oculto logo ao iniciar a pagina;
-conteudo.style.display = 'none';
+conteudo.classList.add('is-visible')
 
 function toggle(){
     
     //armazena o estado do conteudo de false para ativo;
     conteudoIsActive = !conteudoIsActive
 
-    //se o conteudo for ativo ele vai bostrar o bloco;
+    // se o conteudo for ativo ele vai bostrar o bloco;
     if(conteudoIsActive){
-        conteudo.style.display = 'block';
+        // conteudo.style.display = 'block';
+        conteudo.classList.add('is-visible')
         btn.textContent = 'Ocultar'
     // se o conteudo for oculta ele vai mostrar um display none co o botão mostrar;
     }else{
-        conteudo.style.display = 'none';
+        // conteudo.style.display = 'none';
+        conteudo.classList.remove('is-visible')
         btn.textContent = 'Mostrar'
     }
     
 }
-
-// btn.addEventListener('click', toogle)
